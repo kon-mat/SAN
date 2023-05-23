a33f309f3b395dc3773174e993c85151b234645e
Mateusz K <code4well@gmail.com>
2023-05-23 21:03:54 +0200

    TrafficCommon project deleted and files move into TrafficApp

---
+++ Semestr 4/Języki i paradygmaty programowania/TrafficApp/TrafficApp/District.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficApp
+{
+    public class District
+    {
+        private readonly int _id;
+        private readonly string _name;
+        private readonly List<Street> _streets;
+
+        public District(int id, string name)
+        {
+            _id = id;
+            _name = name;
+            _streets = new List<Street>();
+        }
+
+        public int Id { get { return _id; } }
+        public string Name { get { return _name; } }
+        public List<Street> Streets { get { return _streets; } }
+    }
+}
