72a81a84e87b963143be078cb5707cc07d940a50
Mateusz K <code4well@gmail.com>
2023-07-21 20:11:57 +0200

    Small update

--- Semestr 3/Technologie Internetowe/TestowyProjektPS/PSDataJsonCreator/Program.cs
+++ Semestr 3/Technologie Internetowe/TestowyProjektPS/PSDataJsonCreator/Program.cs
@@ -2,8 +2,8 @@
 
 
 
-var source = "D:\\Programowanie\\Programiki\\Visual Studio 2019\\source\\repos\\TestowyProjektPS\\PSDataJsonCreator\\PSDB.txt";
-var dest = "D:\\Programowanie\\Programiki\\Visual Studio 2019\\source\\repos\\TestowyProjektPS\\PSDataJsonCreator\\PSDataBase.json";
+var source = "D:\\Programowanie\\SAN\\Semestr 3\\Technologie Internetowe\\TestowyProjektPS\\PSDataJsonCreator\\PSDB.txt";
+var dest = "D:\\Programowanie\\SAN\\Semestr 3\\Technologie Internetowe\\TestowyProjektPS\\PSDataJsonCreator\\PSDataBase.json";
 var parser = new FileParser(source);
 var movies = parser.GetProblemSolvers();
 var writer = new FileWriter(dest, movies);
